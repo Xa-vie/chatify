ec97e8244070a9111a6896a9e00c2ce618094205
Xavier-Xo <70265592+Xa-vie@users.noreply.github.com>
2021-07-07 13:33:16 +0530

    df

--- src/components/Message.js
+++ src/components/Message.js
@@ -23,16 +23,13 @@ const Message = ({
   uid = '',
 }) => {
   if (!text) return null;
-  const classo = (uid === firebase.auth().currentUser.uid) ? 'sent' : 'received';
+
   const messageClass = uid === firebase.auth().currentUser.uid ? 'flex-row-reverse' : 'flex-row';
   const shift = uid === firebase.auth().currentUser.uid ? 'flex-row-reverse' : '';
   const space = uid === firebase.auth().currentUser.uid ? 'mr-2' : 'ml-2';
-
-
   const messageBodyClass = uid === firebase.auth().currentUser.uid ? 'sent-message-bg text-right' : 'received-message-bg';
   const imageClass = uid === firebase.auth().currentUser.uid ? 'ml-2' : 'mr-2';
-  console.log(messageClass)
-  //
+
   return (
     <div className={`px-4 py-4 rounded-md hover:bg-gray-50 dark:hover:bg-coolDark-600 overflow-hidden flex items-center ${messageClass} `} >
       {/* for image */}
@@ -40,7 +37,7 @@ const Message = ({
         <img
           src={photoURL}
           alt="Avatar"
-          className={`rounded-full mr-4 ${imageClass}`}
+          className={`rounded-full ${imageClass}`}
           width={45}
           height={45}
         />
